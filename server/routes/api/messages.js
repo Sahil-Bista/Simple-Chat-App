166106b8db6bcf713ae7639f23d05ebabbdce1d1
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-10-29 21:15:22 +0545

    Refactor : Added verifyJWT and verifyRoles middlewares to chat controller routes

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -5,14 +5,18 @@ import {
   getChatMembers,
   receiveUserChatMessages,
 } from '../../controllers/chatController.js';
+import { verifyJWT } from '../../middlewares/verifyJWT.js';
+import { verifyRoles } from '../../middlewares/verifyRoles.js';
 
 export const chatRouter = express.Router();
 
 chatRouter.get(
   '/:userId',
+  verifyJWT,
+  verifyRoles('User', 'Admin'),
   getChatHistoryValidation,
   validationResultHandler,
   receiveUserChatMessages
 );
 
-chatRouter.get('/', getChatMembers);
+chatRouter.get('/', verifyJWT, verifyRoles('User', 'Admin'), getChatMembers);
