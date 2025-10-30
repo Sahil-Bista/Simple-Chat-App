4cce0d2b5ae8b29669848c5081e4b37fd5eb8765
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-10-30 17:02:42 +0545

    Feat : added a feature to retrieve user chat history

--- client/src/Pages/Chat.jsx
+++ client/src/Pages/Chat.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Chat = () => {
   const [chatMembers, setChatMembers] = useState([]);
-  // const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const navigate = useNavigate();
+
+  const { userId } = useParams();
+
+  const token = localStorage.getItem('accessToken');
 
   useEffect(() => {
-    const fetchChats = async () => {
+    const fetchChatMembers = async () => {
       try {
-        const token = localStorage.getItem('accessToken');
         const response = await axios.get(
           'http://localhost:3000/api/chat',
           {
@@ -25,37 +30,53 @@ const Chat = () => {
         console.log(err);
       }
     };
-    fetchChats();
-  }, []);
+    fetchChatMembers();
+  }, [token]);
 
-  // useEffect(() => {
-  //   const messageHandler = (message) => {
-  //     setMessages((prev) => [...prev, message]);
-  //   };
-
-  //   socket.on('message', messageHandler);
-
-  //   return () => {
-  //     socket.off('message', messageHandler);
-  //   };
-  // }, []);
+  useEffect(() => {
+    console.log('sup man');
+    console.log(userId);
+    axios
+      .get(
+        `http://localhost:3000/api/chat/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((result) => {
+        console.log('lol', result.data.data);
+        setMessages(result.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, [userId, token]);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <div>
         {chatMembers.length > 0
-          ? chatMembers.map((member, i) => (
-              <ul key={i}>
-                <li>{member.firstName}</li>
+          ? chatMembers.map((member) => (
+              <ul key={member._id}>
+                <button onClick={() => navigate(`/chat/${member._id}`)}>
+                  {member.firstName}
+                </button>
               </ul>
             ))
           : 'No chats made yet'}
       </div>
-      {/* <div>
+      <div>
         {messages.length > 0
-          ? messages.map((msg, i) => <p key={i}>{msg.message}</p>)
-          : 'No messages yet'}
-      </div> */}
+          ? messages.map((message) => (
+              <ul key={message._id}>
+                <li>{message.message}</li>
+              </ul>
+            ))
+          : 'No chats made yet'}
+      </div>
     </div>
   );
 };
